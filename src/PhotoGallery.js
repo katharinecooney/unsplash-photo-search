0403de5490d9fd9c2945ec4388deb3a8c9754cdf
Katharine Cooney <katharinecooney@gmail.com>
2019-08-02 12:10:49 -0500

    Request 24 results from APi instead of 25 for better layout

--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -57,7 +57,7 @@ class PhotoGallery extends Component {
   }
   
   getImages(){
-    axios.get(`https://api.unsplash.com/search/photos?page=2&query=${this.state.searchTerm}&per_page=25`, {headers: {Authorization: 'Client-ID 282d6101c992fd476700cb4c1f429cee107dee704a8056775b945fd16126d59b'}})
+    axios.get(`https://api.unsplash.com/search/photos?page=2&query=${this.state.searchTerm}&per_page=24`, {headers: {Authorization: 'Client-ID 282d6101c992fd476700cb4c1f429cee107dee704a8056775b945fd16126d59b'}})
     .then(response => this.setState({
       images: response.data.results,
       hasLoadedImages: true
@@ -114,7 +114,7 @@ class PhotoGallery extends Component {
          
           
           {
-          this.state.hasLoadedImages && 
+          (this.state.hasLoadedImages || this.state.requestedSavedPhotos) && 
           <div className="PhotoGallery-scroll">
             <a href="#top" onClick={this.scrollUp}>
             <img src={chevron} alt="scroll up arrow"></img>
